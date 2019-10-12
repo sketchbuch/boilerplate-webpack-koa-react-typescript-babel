814fb918a0bdb8f7c67796448755854ddf280ab1
SketchBuch <sb.design.de@gmail.com>
2019-10-12 13:54:38 +0200

    added stylelint for style components

--- src/common/components/App/App.styles.ts
+++ src/common/components/App/App.styles.ts
@@ -4,9 +4,11 @@ export const StyledApp = styled.div`
   text-align: center;
   font-size: 2rem;
 `;
+StyledApp.displayName = 'StyledApp';
 
 export const StyledP = styled.div`
   text-align: center;
   font-weight: bold;
   font-size: 1rem;
 `;
+StyledP.displayName = 'StyledP';
